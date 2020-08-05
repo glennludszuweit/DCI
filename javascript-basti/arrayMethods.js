9b7348a621bc3514618705c1b01bfb6fad2115fc
glenn <glenn.ludszuweit@gmail.com>
2020-08-05 12:24:08 +0200

    basti js basics arraz methods exercise

--- javascript-basti/arrayMethods.js
+++ javascript-basti/arrayMethods.js
@@ -1,19 +1,59 @@
+console.log('***********************function()***********************');
 let counter = 0;
 let numVal = 3;
-
-function addOne(theParams, secondParams) {
+function oldAddOne(theParams, secondParams) {
   return (theParams + 1) + (secondParams + 2);
 }
-addOne(counter, numVal);
+oldAddOne(counter, numVal);
 
-///*****************************************.reduce()*********
-let classGrades = [2, 1, 3, 4, 4, 6, 1, 3, 5];
-let totalGrade = classGrades.reduce(addGrades, 0);
- 
-function addGrades(gradeTotal, value) {
-  return gradeTotal + value
+function addOne( theParameter ) {
+  return theParameter + 1;
 }
+console.log(addOne(2));
+let doAddOne = addOne;
+console.log(doAddOne(2))
 
+let multiplyByTwo = (number) => {
+  let result = number * 2;
+  return result;
+}
+multiplyByTwo(37)
+
+let squareRoot = (sqrtParam) => {
+  let result = Math.sqrt(sqrtParam)
+  return result;
+}
+squareRoot(25);
+
+let secondPowerOfSum = (num1, num2) => {
+  let result = (num1 + num2) ** 2;
+  return result;
+}
+secondPowerOfSum(13, 15);
+
+let addQuestionMarkToStr = (frageStr) => {
+  let result = frageStr + '?';
+  return result;
+}
+addQuestionMarkToStr('What');
+
+let volume = (side) => {
+  let result = Math.pow(side, 3); //side ** 3;
+  return result;
+}
+volume(10);
+
+let negativeNum = (number) => {
+  let result =  Math.abs(number) * -1;
+  return result;
+}
+negativeNum(65);
+
+console.log('***********************.reduce()***********************');
+let classGrades = [2, 1, 3, 4, 4, 6, 1, 3, 5];
+let totalGrade = classGrades.reduce((gradeTotal, value) => {
+  return gradeTotal + value
+}, 0);
 console.log(totalGrade / classGrades.length)
 
 let newNames = ['John', 'Jane', 'Sam', 'Bob', 'Alice'];
@@ -22,7 +62,16 @@ let newMessage = newNames.reduce((greetingMessage, name) => {
 }, 'Welcome ')
 console.log(newMessage + 'to DCI');
 
-///*****************************************.forEach()*********
+let reduceStrArray = ['Glenn', 'Tony'];
+let reduceResultStr = reduceStrArray.reduce((result, value) => {
+  if (value.length > result.length) {
+    result = value;
+  }
+  return result
+},  '')
+console.log(reduceResultStr);
+
+console.log('***********************.forEach()***********************');
 let classNames = ['Daniel', 'Danylo', 'Glenn', 'Basti'];
 classNames.forEach((name) => {
   console.log(`Hi ${name}`);
@@ -38,14 +87,32 @@ console.log(sum / myNums.length)
 let allNames = ['John', 'Jane', 'Sam', 'Bob', 'Alice'];
 let message = 'Welcome ';
 allNames.forEach((name) => {
-  message = message + name + ' ';
+  message = message + name + ', ';
 })
-console.log(message + ' to DCI');
+console.log(message + 'to DCI');
 
-///*****************************************.map()*********
+let foreachStrArray = ['Glenn', 'Tony'];
+let foreachResult = '';
+foreachStrArray.forEach((longest) => {
+  if (longest.length > foreachResult.length) {
+    foreachResult = longest;
+  }
+});
+console.log(foreachResult)
+
+let forEachNums = [1,2,3,4,5];
+let forEachLowestNum = Infinity;
+forEachNums.forEach((number) => {
+  if (number < forEachLowestNum) {
+    forEachLowestNum = number
+  }
+})
+console.log(forEachLowestNum)
+
+console.log('***********************.map()***********************');
 let allPrices = [21.3, 55.45, 34.5, 10.02, 9.99];
 let newPrices = allPrices.map((prices) => {
-  return Math.ceil((prices / 1.19) * 1.16) - .05;
+  return Math.round((prices / 1.19) * 1.16) - .05;
 })
 console.log(newPrices)
 
